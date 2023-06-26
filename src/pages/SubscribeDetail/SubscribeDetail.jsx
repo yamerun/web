5f3270aac84ee03dc0a70b0dd6400bdf7599669f
Your Name <you@example.com>
2023-06-26 06:58:45 +0300

    changhes

--- src/pages/SubscribeDetail/SubscribeDetail.jsx
+++ src/pages/SubscribeDetail/SubscribeDetail.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 
-import style from './SubscribeDetail.module.scss'
+import style from "./SubscribeDetail.module.scss";
 
-
-export default function SubscribeDetail  ()  {
-    return (
-  <div>
-    
-  </div>
-    )
-} 
+export default function SubscribeDetail() {
+  return (
+    <div className={style.subscribe}>
+      <h1 className={style.subscribe__heading}>ПОДПИСКА</h1>
+      <div className={style.subscribe__container}>
+        <article className={style.subscribe__container__block}>
+          <h1 className={style.subscribe__container__block__heading}>
+            до конца подписки осталось
+          </h1>
+          30 дней 18 часов 42 минуты
+          <h1 className={style.subscribe__container__block__heading}>Инструкция по оплате:</h1>
+          <p className={style.subscribe__container__text}>
+            взять деньги положить на карту сбер альфа тинькофф нажать оплатить и
+            оплатить после этого оплата будет считаться успешной и вы оплатите
+            подписку
+          </p>
+          <button className={style.subscribe__container__block__btn}>
+            Оплатить
+          </button>
+        </article>
+      </div>
+    </div>
+  );
+}
