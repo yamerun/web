59e72eb981d6e812b1e91d6604231d34b70dd371
yamerun <yarwizard@gmail.com>
2023-07-19 19:58:45 +0500

    FIX: delete logo from Login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -7,61 +7,55 @@ import { useNavigate } from "react-router-dom";
 import logo from "../../assets/imgs/changePro.png";
 import { Input } from "../../components/InputLogin/Input";
 export const LoginPage = () => {
-  const navigate = useNavigate();
-  const [err, setErr] = useState("");
-  const { email, password } = useSelector((state) => ({
-    email: state.AccountSlice.emailLogin,
-    password: state.AccountSlice.passwordLogin,
-  }));
+	const navigate = useNavigate();
+	const [err, setErr] = useState("");
+	const { email, password } = useSelector((state) => ({
+		email: state.AccountSlice.emailLogin,
+		password: state.AccountSlice.passwordLogin,
+	}));
 
-  const Login = () => {
-    axios
-      .post(`https://change.pro/api/auth/login`, {
-        email: email,
-        password: password,
-      })
-      .then(function (response) {
-        localStorage.setItem("jwt", response.data.token);
-        navigate("/account");
-      })
-      .catch(function (error) {
-        setErr(error.response.data.message);
-      });
-  };
+	const Login = () => {
+		axios
+			.post(`https://change.pro/api/auth/login`, {
+				email: email,
+				password: password,
+			})
+			.then(function (response) {
+				localStorage.setItem("jwt", response.data.token);
+				navigate("/account");
+			})
+			.catch(function (error) {
+				setErr(error.response.data.message);
+			});
+	};
 
-  const goMain = () => {
-    navigate("/");
-  };
+	const goMain = () => {
+		navigate("/");
+	};
 
-  return (
-    <div className={style.LoginPage}>
-      <div className={style.LoginPage__formBox}>
-        <img
-          alt="logo"
-          src={logo}
-          className={style.LoginPage__logo}
-          onClick={goMain}
-        />
-        <div className={style.LoginPage__form}>
-          <h1 className={style.LoginPage__header}>Авторизация</h1>
-          <Input />
-          <button className={style.LoginPage__btn} onClick={Login}>
-            Войти →
-          </button>
-          <span style={{ color: "white" }}>{err}</span>
-          <div className={style.LoginPage__goToRegister}>
-            <Link
-              className={style.LoginPage__goToRegister__link}
-              to="/register"
-            >
-              Зарегистрироваться
-            </Link>
-            <h1 className={style.LoginPage__goToRegister__header}>
-              Если вы новый пользователь
-            </h1>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={style.LoginPage}>
+			<div className={style.LoginPage__formBox}>
+				<div className={style.LoginPage__form}>
+					<h1 className={style.LoginPage__header}>Авторизация</h1>
+					<Input />
+					<button className={style.LoginPage__btn} onClick={Login}>
+						Войти →
+					</button>
+					<span style={{ color: "white" }}>{err}</span>
+					<div className={style.LoginPage__goToRegister}>
+						<Link
+							className={style.LoginPage__goToRegister__link}
+							to="/register"
+						>
+							Зарегистрироваться
+						</Link>
+						<h1 className={style.LoginPage__goToRegister__header}>
+							Если вы новый пользователь
+						</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
