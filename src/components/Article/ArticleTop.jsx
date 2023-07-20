347a2318e472ca3d7edf0979c092590864c1f511
yamerun <yarwizard@gmail.com>
2023-07-21 00:37:35 +0500

    UPD: add header text on Main Page

---
+++ src/components/Article/ArticleTop.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import style from "./Article.module.scss";
+import axios from "axios";
+export const ArticleTop = () => {
+	const [header, setHeader] = useState("");
+	const [text, setText] = useState("");
+	useEffect(() => {
+		axios
+			.get(`https://change.pro/api/content/get`)
+			.then(function (response) {
+				console.log(response.data.data.main_page_top.title_description);
+				setText(response.data.data.main_page_top.description);
+				setHeader(response.data.data.main_page_top.title_description);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}, []);
+
+	function createMarkup(text) {
+		return { __html: `${text}` };
+	}
+
+	return (
+		<div className={style.Article}>
+			<h1 className={style.Article__header}> {header}</h1>
+			<div
+				style={{ color: 'white' }}
+				className={style.Article__text}
+				dangerouslySetInnerHTML={createMarkup(text)}
+			></div>
+		</div>
+	);
+};
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,17 +4,19 @@ import { SearchMenu } from "../../components/SearchMenu/SearchMenu";
 //import { Carousel } from "../../components/Advertisement/Carousel";
 import { Fillters } from "../../components/Fillters/Fillters";
 import { Article } from "../../components/Article/Article";
+import { ArticleTop } from "../../components/Article/ArticleTop";
 
 export const MainPage = () => {
-  return (
-    <div className={style.MainPage}>
-      <div className={style.MainPage__containerMenu}>
-        <SearchMenu />
-          <div className={style.MainPage__rightMenu}>
-            <Fillters />
-            <Article />
-          </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={style.MainPage}>
+			<div className={style.MainPage__containerMenu}>
+				<SearchMenu />
+				<div className={style.MainPage__rightMenu}>
+					<ArticleTop />
+					<Fillters />
+					<Article />
+				</div>
+			</div>
+		</div>
+	);
 };
