0db80092ca70526e7d0b7eb10c5bf7a2d6089407
yamerun <yarwizard@gmail.com>
2023-09-07 23:41:52 +0500

    FIX: Account Exchanger styles for Marks & Scammers

--- src/components/MarksPop/MarksPop.jsx
+++ src/components/MarksPop/MarksPop.jsx
@@ -3,184 +3,188 @@ import style from "./Markspop.module.scss";
 import axios from "axios";
 import { Checkbox } from "../CheckBoxes/Checkbox";
 export const MarkPop = ({ prop, setSuccess }) => {
-  const [marks, setMarks] = useState([]);
-  const [value, setValue] = useState("");
-  const [value2, setValue2] = useState("");
-  const [searchResult, setResult] = useState([]);
-  const [searchresult2, setResult2] = useState([]);
-  const [variants, setVariants] = useState([]);
-  const [variants2, setVariants2] = useState([]);
-  const [from, setfrom] = useState("");
-  const [to, setTo] = useState("");
-  const [mark, setMark] = useState("");
+	const [marks, setMarks] = useState([]);
+	const [value, setValue] = useState("");
+	const [value2, setValue2] = useState("");
+	const [searchResult, setResult] = useState([]);
+	const [searchresult2, setResult2] = useState([]);
+	const [variants, setVariants] = useState([]);
+	const [variants2, setVariants2] = useState([]);
+	const [from, setfrom] = useState("");
+	const [to, setTo] = useState("");
+	const [mark, setMark] = useState("");
 
-  const config = {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-      "Content-Type": "application/json",
-    },
-  };
+	const config = {
+		headers: {
+			Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+			"Content-Type": "application/json",
+		},
+	};
 
-  useEffect(() => {
-    axios
-      .get(`https://change.pro/api/marks/types`, config)
-      .then(function (response) {
-        setMarks(response.data.data);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get(`https://change.pro/api/marks/types`, config)
+			.then(function (response) {
+				setMarks(response.data.data);
+			});
+	}, []);
 
-  const getFromValue = (e) => {
-    setValue(e.target.value);
-  };
+	const getFromValue = (e) => {
+		setValue(e.target.value);
+	};
 
-  const getToValue = (e) => {
-    setValue2(e.target.value);
-  };
+	const getToValue = (e) => {
+		setValue2(e.target.value);
+	};
 
-  useEffect(() => {
-    axios
-      .get(`https://change.pro/api/exchangers/currencies/list`)
-      .then(function (response) {
-        setVariants(response.data.data);
-      })
-      .catch(function (error) {});
-  }, []);
+	useEffect(() => {
+		axios
+			.get(`https://change.pro/api/exchangers/currencies/list`)
+			.then(function (response) {
+				setVariants(response.data.data);
+			})
+			.catch(function (error) { });
+	}, []);
 
-  useEffect(() => {
-    axios
-      .get(`https://change.pro/api/exchangers/currencies/list`)
-      .then(function (response) {
-        setVariants2(response.data.data);
-      })
-      .catch(function (error) {});
-  }, []);
+	useEffect(() => {
+		axios
+			.get(`https://change.pro/api/exchangers/currencies/list`)
+			.then(function (response) {
+				setVariants2(response.data.data);
+			})
+			.catch(function (error) { });
+	}, []);
 
-  useEffect(() => {
-    setResult(
-      variants.filter((item) =>
-        item.currency.toLowerCase().includes(value.toLocaleLowerCase())
-      )
-    );
-  }, [value, variants]);
+	useEffect(() => {
+		setResult(
+			variants.filter((item) =>
+				item.currency.toLowerCase().includes(value.toLocaleLowerCase())
+			)
+		);
+	}, [value, variants]);
 
-  useEffect(() => {
-    setResult2(
-      variants2.filter((item) =>
-        item.currency.toLowerCase().includes(value2.toLocaleLowerCase())
-      )
-    );
-  }, [value2, variants2]);
+	useEffect(() => {
+		setResult2(
+			variants2.filter((item) =>
+				item.currency.toLowerCase().includes(value2.toLocaleLowerCase())
+			)
+		);
+	}, [value2, variants2]);
 
-  const setFromValue = (e) => {
-    setfrom(e.target.textContent);
-    setResult([]);
-  };
+	const setFromValue = (e) => {
+		setfrom(e.target.textContent);
+		document.getElementById('getFrom').value = e.target.textContent;
+		setResult([]);
+	};
 
-  const setToValue = (e) => {
-    setTo(e.target.textContent);
-    setResult2([]);
-  };
+	const setToValue = (e) => {
+		setTo(e.target.textContent);
+		document.getElementById('getTo').value = e.target.textContent;
+		setResult2([]);
+	};
 
-  const getMark = (e) => {
-    setMark(e.target.id);
-    const btnElements = document.querySelectorAll(
-      `.${style.Marks__content__body__item}`
-    );
-    e.target.classList.add(`${style.active}`);
-    for (let i of btnElements) {
-      if (i != e.target) {
-        i.classList.remove(`${style.active}`);
-      }
-    }
-  };
+	const getMark = (e) => {
+		setMark(e.target.id);
+		const btnElements = document.querySelectorAll(
+			`.${style.Marks__content__body__item}`
+		);
+		e.target.classList.add(`${style.active}`);
+		for (let i of btnElements) {
+			if (i != e.target) {
+				i.classList.remove(`${style.active}`);
+			}
+		}
+	};
 
-  const data = {
-    from: from,
-    to: to,
-    type_id: mark,
-  };
+	const data = {
+		from: from,
+		to: to,
+		type_id: mark,
+	};
 
-  const addToMark = () => {
-    axios
-      .post(`https://change.pro/api/marks/create`, data, config)
-      .then(function (response) {
-        setSuccess(true);
-      })
-      .catch(function (error) {});
-    prop();
-  };
+	const addToMark = () => {
+		axios
+			.post(`https://change.pro/api/marks/create`, data, config)
+			.then(function (response) {
+				setSuccess(true);
+			})
+			.catch(function (error) { });
+		prop();
+	};
 
-  const closePop = useCallback(() => {
-    prop();
-  }, []);
+	const closePop = useCallback(() => {
+		prop();
+	}, []);
 
-  console.log(marks)
-  return (
-    <div className={style.Marks}>
-      <div className={style.Marks__content}>
-        <div className={style.Marks__content__header}>
-          <div className={style.Marks__content__header__box}>
-            <input
-              className={style.Marks__content__header__input}
-              placeholder="Отдаете"
-              onChange={(e) => getFromValue(e)}
-            />
-            {value.length != 0 && searchResult.length != 0 && (
-              <div className={style.Marks__content__header__variants}>
-                {searchResult.map((item) => (
-                  <p onClick={(e) => setFromValue(e)}>{item.currency}</p>
-                ))}
-              </div>
-            )}
-          </div>
-          <div className={style.Marks__content__header__box}>
-            <input
-              className={style.Marks__content__header__input}
-              placeholder="Получаете"
-              onChange={(e) => getToValue(e)}
-            />
-            {value2.length != 0 && searchresult2.length != 0 && (
-              <div className={style.Marks__content__header__variants}>
-                {searchresult2.map((item) => (
-                  <p onClick={(e) => setToValue(e)}>{item.currency}</p>
-                ))}
-              </div>
-            )}
-          </div>
-        </div>
-        <div className={style.Marks__content__body}>
-          {marks.map((item) => (
-            <div key={item.id} className={style.Marks__content__body__boxItem}>
-              <img
-                src={`https://change.pro${item.icon.path}`}
-                className={style.Marks__content__body__img}
-              />
-              <p
-                className={style.Marks__content__body__item}
-                id={item.id}
-                onClick={(e) => getMark(e)}
-              >
-                {item.id}: {item.description}
-              </p>
-            </div>
-          ))}
-        </div>
-        <div></div>
-        <div className={style.Marks__content__footer}>
-          <button
-            onClick={addToMark}
-            className={style.Marks__content__footer__addBtn}
-          >
-            Добавить метку
-          </button>
-          <button
-            onClick={closePop}
-            className={style.Marks__content__footer__abort}
-          >
-            отмена
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	console.log(marks)
+	return (
+		<div className={style.Marks}>
+			<div className={style.Marks__content}>
+				<div className={style.Marks__content__header}>
+					<div className={style.Marks__content__header__box}>
+						<input
+							id="getFrom"
+							className={style.Marks__content__header__input}
+							placeholder="Отдаете"
+							onChange={(e) => getFromValue(e)}
+						/>
+						{value.length != 0 && searchResult.length != 0 && (
+							<div className={style.Marks__content__header__variants}>
+								{searchResult.map((item) => (
+									<p onClick={(e) => setFromValue(e)}>{item.currency}</p>
+								))}
+							</div>
+						)}
+					</div>
+					<div className={style.Marks__content__header__box}>
+						<input
+							id="getTo"
+							className={style.Marks__content__header__input}
+							placeholder="Получаете"
+							onChange={(e) => getToValue(e)}
+						/>
+						{value2.length != 0 && searchresult2.length != 0 && (
+							<div className={style.Marks__content__header__variants}>
+								{searchresult2.map((item) => (
+									<p onClick={(e) => setToValue(e)}>{item.currency}</p>
+								))}
+							</div>
+						)}
+					</div>
+				</div>
+				<div className={style.Marks__content__body}>
+					{marks.map((item) => (
+						<div key={item.id} className={style.Marks__content__body__boxItem}>
+							<img
+								src={`https://change.pro${item.icon.path}`}
+								className={style.Marks__content__body__img}
+							/>
+							<p
+								className={style.Marks__content__body__item}
+								id={item.id}
+								onClick={(e) => getMark(e)}
+							>
+								{item.id}: {item.description}
+							</p>
+						</div>
+					))}
+				</div>
+				<div></div>
+				<div className={style.Marks__content__footer}>
+					<button
+						onClick={addToMark}
+						className={style.Marks__content__footer__addBtn}
+					>
+						Добавить метку
+					</button>
+					<button
+						onClick={closePop}
+						className={style.Marks__content__footer__abort}
+					>
+						отмена
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
--- src/components/ScammersPop/ScammersPop.jsx
+++ src/components/ScammersPop/ScammersPop.jsx
@@ -4,141 +4,141 @@ import style from "./ScammersPop.module.scss";
 import { useCallback } from "react";
 
 export const ScammersPop = ({ props, setUpdate }) => {
-  const [name, setName] = useState("");
-  const [contacts, setContacts] = useState("");
-  const [walletId, setWalletId] = useState("");
-  const [description, setDescription] = useState("");
-  const [type, setType] = useState("");
-  const [variants, setVariants] = useState([]);
-  const [active, setActive] = useState(false);
+	const [name, setName] = useState("");
+	const [contacts, setContacts] = useState("");
+	const [walletId, setWalletId] = useState("");
+	const [description, setDescription] = useState("");
+	const [type, setType] = useState("");
+	const [variants, setVariants] = useState([]);
+	const [active, setActive] = useState(false);
 
-  const changeName = (e) => {
-    setName(e.target.value);
-  };
+	const changeName = (e) => {
+		setName(e.target.value);
+	};
 
-  const changeContacts = (e) => {
-    setContacts(e.target.value);
-  };
-  const changeWalletId = (e) => {
-    setWalletId(e.target.value);
-  };
-  const changeDescription = (e) => {
-    setDescription(e.target.value);
-  };
+	const changeContacts = (e) => {
+		setContacts(e.target.value);
+	};
+	const changeWalletId = (e) => {
+		setWalletId(e.target.value);
+	};
+	const changeDescription = (e) => {
+		setDescription(e.target.value);
+	};
 
-  const config = {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-      "Content-Type": "application/json",
-    },
-  };
+	const config = {
+		headers: {
+			Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+			"Content-Type": "application/json",
+		},
+	};
 
-  const data = {
-    scam_name: name,
-    scam_email: contacts,
-    scam_wallet: walletId,
-    scam_type_id: type,
-    scam_description: description,
-  };
+	const data = {
+		scam_name: name,
+		scam_email: contacts,
+		scam_wallet: walletId,
+		scam_type_id: type,
+		scam_description: description,
+	};
 
-  const sendNewScamInfo = () => {
-    axios
-      .post(`https://change.pro/api/scammers/create`, data, config)
-      .then(function (response) {
-        setUpdate(true);
-        props(false);
-      });
-  };
-  const changeType = (e) => {
-    setType(e.target.id);
-    setActive(false);
-  };
+	const sendNewScamInfo = () => {
+		axios
+			.post(`https://change.pro/api/scammers/create`, data, config)
+			.then(function (response) {
+				setUpdate(true);
+				props(false);
+			});
+	};
+	const changeType = (e) => {
+		setType(e.target.id);
+		setActive(false);
+	};
 
-  const getSelectVariants = () => {
-    axios
-      .get(`https://change.pro/api/scammers/types`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-        },
-      })
-      .then(function (response) {
-        setVariants(response.data.data);
-      });
-    setActive(!active);
-  };
+	const getSelectVariants = () => {
+		axios
+			.get(`https://change.pro/api/scammers/types`, {
+				headers: {
+					Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+				},
+			})
+			.then(function (response) {
+				setVariants(response.data.data);
+			});
+		setActive(!active);
+	};
 
-  const close = useCallback(() => {
-    props(false);
-  }, []);
+	const close = useCallback(() => {
+		props(false);
+	}, []);
 
-  return (
-    <div className={style.Modal} style={{ height: `${window.outerHeight}` }}>
-      <div className={style.Modal__container}>
-        <div className={style.Modal__container__header}>
-          <h1 className={style.Modal__container__header__tittle}>
-            Новая запись
-          </h1>
-          <button
-            className={style.Modal__container__header__btn}
-            onClick={close}
-          ></button>
-        </div>
-        <div className={style.Modal__container__inputMenu}>
-          <div className={style.Modal__container__inputBox}>
-            <h1>Имя</h1>
-            <input
-              className={style.Modal__container__inputBox__input}
-              onChange={(e) => changeName(e)}
-            />
-          </div>
-          <div className={style.Modal__container__inputBox}>
-            <h1>Email</h1>
-            <input
-              className={style.Modal__container__inputBox__input}
-              onChange={(e) => changeContacts(e)}
-            />
-          </div>
-          <div className={style.Modal__container__inputBox}>
-            <h1>Кошелек</h1>
-            <input
-              className={style.Modal__container__inputBox__input}
-              onChange={(e) => changeWalletId(e)}
-            />
-          </div>
+	return (
+		<div className={style.Modal} style={{ height: `${window.outerHeight}` }}>
+			<div className={style.Modal__container}>
+				<div className={style.Modal__container__header}>
+					<h4 className={style.Modal__container__header__tittle}>
+						Новая запись
+					</h4>
+					<button
+						className={style.Modal__container__header__btn}
+						onClick={close}
+					></button>
+				</div>
+				<div className={style.Modal__container__inputMenu}>
+					<div className={style.Modal__container__inputBox}>
+						<label>Имя</label>
+						<input
+							className={style.Modal__container__inputBox__input}
+							onChange={(e) => changeName(e)}
+						/>
+					</div>
+					<div className={style.Modal__container__inputBox}>
+						<label>Email</label>
+						<input
+							className={style.Modal__container__inputBox__input}
+							onChange={(e) => changeContacts(e)}
+						/>
+					</div>
+					<div className={style.Modal__container__inputBox}>
+						<label>Кошелек</label>
+						<input
+							className={style.Modal__container__inputBox__input}
+							onChange={(e) => changeWalletId(e)}
+						/>
+					</div>
 
-          <div className={style.Modal__container__inputBox}>
-            <h1>Тип</h1>
-            <div
-              onClick={getSelectVariants}
-              className={style.Modal__container__inputBox__input}
-            >
-              {type}
-            </div>
-            {active && (
-              <ul className={style.Modal__variants}>
-                {variants.map((item) => (
-                  <li onClick={(e) => changeType(e)} key={item.id} id={item.id}>
-                    {item.type}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-          <div className={style.Modal__container__inputBox}>
-            <h1>Описание</h1>
-            <textarea
-              className={style.Modal__container__inputBox__textarea}
-              onChange={(e) => changeDescription(e)}
-            />
-          </div>
-          <button
-            className={style.Modal__container__btn}
-            onClick={sendNewScamInfo}
-          >
-            Создать
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+					<div className={style.Modal__container__inputBox}>
+						<label>Тип</label>
+						<div
+							onClick={getSelectVariants}
+							className={style.Modal__container__inputBox__input}
+						>
+							{type}
+						</div>
+						{active && (
+							<ul className={style.Modal__variants}>
+								{variants.map((item) => (
+									<li onClick={(e) => changeType(e)} key={item.id} id={item.id}>
+										{item.type}
+									</li>
+								))}
+							</ul>
+						)}
+					</div>
+					<div className={style.Modal__container__inputBox}>
+						<label>Описание</label>
+						<textarea
+							className={style.Modal__container__inputBox__textarea}
+							onChange={(e) => changeDescription(e)}
+						/>
+					</div>
+					<button
+						className={style.Modal__container__btn}
+						onClick={sendNewScamInfo}
+					>
+						Создать
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
