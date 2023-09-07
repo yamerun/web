e7429fab3849b608e973a48ba36bda6797beb6aa
yamerun <yarwizard@gmail.com>
2023-09-07 23:43:01 +0500

    UPD: Search exchangers by name

--- src/pages/AllExchangersPage/AllEchangers.jsx
+++ src/pages/AllExchangersPage/AllEchangers.jsx
@@ -11,7 +11,7 @@ import { SearchMenu } from "../../components/SearchMenu/SearchMenu";
 import validateInputValueForSearch from "./js/searchValidatorhelper";
 export const exchangersLoader = async () => {
 	const res = await fetch(
-		`https://change.pro/api/exchangers/currencies/get?orderBy=amount&sort=asc`
+		`https://change.pro/api/exchangers/currencies/get?orderBy=exchanger_name&sort=asc`
 	);
 	const item = await res.json();
 	return { item };
@@ -21,32 +21,17 @@ export const AllEchangers = () => {
 	const { item } = useLoaderData();
 	const [result, setResultExchangers] = useState([]);
 	const [inputVal, setInputVal] = useState("");
-	const [inputVal2, setInputVal2] = useState("");
-	const [variants, setVariants] = useState([]);
-	const [variants2, setVariants2] = useState([]);
 	const [searchResult, setSearchResult] = useState([]);
-	const [searchResult2, setSearchResult2] = useState([]);
 	const [selected, setSelected] = useState([]);
-	const [selected2, setSelected2] = useState([]);
 	const ref = useRef(null);
-	const ref2 = useRef(null);
-
-	const { currentTo, currentFrom } = useSelector((state) => ({
-		currentTo: state.itemsSlice.currentTo,
-		currentFrom: state.itemsSlice.currentFrom,
-	}));
 
 	const changeValueForSearch = (e) => {
 		const newVal = validateInputValueForSearch(e.target.value);
 		setInputVal(newVal);
 	};
-	const changeValue2ForSearch = (e) => {
-		const newVal = validateInputValueForSearch(e.target.value);
-		setInputVal2(newVal);
-	};
 
 	useEffect(() => {
-		if (selected.length == 0) {
+		if (inputVal.length < 2) {
 			setResultExchangers(item.data);
 			const getCurrenciesAll = setInterval(() => {
 				axios
@@ -59,75 +44,25 @@ export const AllEchangers = () => {
 			}, 5000);
 			return () => clearInterval(getCurrenciesAll);
 		}
-	}, [item, selected, selected2]);
+	}, [item, inputVal]);
 
 	useEffect(() => {
-		axios
-			.get(`https://change.pro/api/exchangers/currencies/list`)
-			.then(function (response) {
-				setVariants(response.data.data);
-			})
-			.catch(function (error) { });
-	}, []);
-
-	useEffect(() => {
-		axios
-			.get(`https://change.pro/api/exchangers/currencies/list`)
-			.then(function (response) {
-				setVariants2(response.data.data);
-			})
-			.catch(function (error) { });
-	}, []);
-
-	const openItemSite = (url) => {
-		window.open(`${url}`);
-	};
-
-	useEffect(() => {
-		setSearchResult(
-			variants.filter((item) =>
-				item.currency.toLowerCase().includes(inputVal.toLocaleLowerCase())
-			)
-		);
-	}, [inputVal, variants]);
-
-	useEffect(() => {
-		setSearchResult2(
-			variants2.filter((item) =>
-				item.currency.toLowerCase().includes(inputVal2.toLocaleLowerCase())
-			)
-		);
-	}, [inputVal2, variants2]);
-
-	const currencyFromMainMenu = React.useMemo(() => currentFrom, [currentFrom]);
-	const currencyToMainMenu = React.useMemo(() => currentTo, [currentTo]);
-
-	useEffect(() => {
-		if (currencyFromMainMenu != undefined && currencyToMainMenu != undefined) {
+		if (inputVal.length > 1) {
 			axios
 				.get(
-					`https://change.pro/api/exchangers/currencies/get?orderBy=out&sort=desc&from=${currencyFromMainMenu}&to=${currencyToMainMenu}&limit=50`
+					`https://change.pro/api/exchangers/currencies/get?orderBy=amount&sort=asc&exchanger_name=${inputVal}&limit=50`
 				)
 				.then(function (response) {
 					setResultExchangers(response.data.data);
 				})
-				.then(function (response) { })
 				.catch(function (error) { });
 		}
-		axios
-			.get(
-				`https://change.pro/api/exchangers/currencies/get?orderBy=out&sort=desc&from=${selected}&to=${selected2}&limit=50`
-			)
-			.then(function (response) {
-				setResultExchangers(response.data.data);
-			})
-			.then(function (response) { })
-			.catch(function (error) { });
+
 		const get = setInterval(() => {
-			if (selected != "" && selected2 != "undefined") {
+			if (inputVal.length > 1) {
 				axios
 					.get(
-						`https://change.pro/api/exchangers/currencies/get?orderBy=out&sort=desc&from=${selected}&to=${selected2}&limit=50`
+						`https://change.pro/api/exchangers/currencies/get?orderBy=amount&sort=asc&exchanger_name=${inputVal}&limit=50`
 					)
 					.then(function (response) {
 						setResultExchangers(response.data.data);
@@ -138,24 +73,17 @@ export const AllEchangers = () => {
 		}, 3000);
 
 		return () => clearInterval(get);
-	}, [selected, selected2, currencyFromMainMenu, currencyToMainMenu]);
+	}, [inputVal]);
 
 	const setFrom = (e) => {
 		setSelected(e.target.textContent);
-	};
-
-	const setTo = (e) => {
-		setSelected2(e.target.textContent);
 	};
 
 	useEffect(() => {
 		if (selected.length != 0 && ref.current != null) {
 			ref.current.classList.add(`${style.hide}`);
 		}
-		if (selected2.length != 0 && ref2.current != null) {
-			ref2.current.classList.add(`${style.hide}`);
-		}
-	}, [selected, selected2]);
+	}, [selected]);
 
 	return (
 		<div className={'section-wrapper'}>
@@ -178,7 +106,7 @@ export const AllEchangers = () => {
 													className={
 														style.Exchangers__content__body__seacrh__input__field
 													}
-													placeholder="Отдаете"
+													placeholder="Название обменника"
 													name="currencyFrom"
 												/>
 												<button
@@ -200,61 +128,7 @@ export const AllEchangers = () => {
 												</div>
 											)}
 										</div>
-										<div
-											className={
-												style.Exchangers__content__body__seacrh__fields__separate
-											}
-										></div>
-										<div className={style.Exchangers__content__body__seacrh__input}>
-											<div
-												className={
-													style.Exchangers__content__body__seacrh__input__controlls
-												}
-											>
-												<input
-													onChange={(e) => changeValue2ForSearch(e)}
-													className={
-														style.Exchangers__content__body__seacrh__input__field
-													}
-													placeholder="Получаете"
-													name="currencyTo"
-												/>
-												<button
-													className={
-														style.Exchangers__content__body__seacrh__input__btn
-													}
-												/>
-											</div>
-											{inputVal2.length != 0 && searchResult2.length != 0 && (
-												<div
-													ref={ref2}
-													className={
-														style.Exchangers__content__body__seacrh__input__results
-													}
-												>
-													{searchResult2.map((item) => (
-														<h1 onClick={(e) => setTo(e)}>{item.currency}</h1>
-													))}
-												</div>
-											)}
-										</div>
 									</div>
-								</div>
-								<div className={style.Exchangers__content__body__seacrh__selected}>
-									<span
-										className={style.Exchangers__content__body__seacrh__selected__name}
-									>
-										{currencyFromMainMenu != ""
-											? `${currencyFromMainMenu}`
-											: `${selected}`}
-									</span>
-									<span
-										className={style.Exchangers__content__body__seacrh__selected__name}
-									>
-										{currencyToMainMenu != ""
-											? `${currencyToMainMenu}`
-											: `${selected2}`}
-									</span>
 								</div>
 								<table className={style.Exchangers__content__table}>
 									<thead>
